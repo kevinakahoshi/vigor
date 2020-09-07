a79b287e1dce55387e4ac10072a385d75cf61bb1
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-09-07 21:50:10 +0000

    Removed unused component

---
+++ client/theme/secondaryButtonStyles.js
@@ -0,0 +1,23 @@
+import {
+  withStyles
+} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const VigorSecondaryButton = withStyles({
+  root: {
+    padding: '.5rem 3rem',
+    border: 0,
+    backgroundImage: 'linear-gradient(#ffffff, #ffffff)',
+    boxShadow: '0rem .5rem 1rem rgba(0, 0, 0, .25)',
+    '&:hover': {
+      boxShadow: '0rem .25rem .5rem rgba(0, 0, 0, .25)'
+    }
+  },
+  label: {
+    backgroundImage: 'linear-gradient(45deg, #4BBCC0, #8AFFC7)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent'
+  }
+})(Button);
+
+export default VigorSecondaryButton;
--- client/views/Home.jsx
+++ client/views/Home.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import {
-  Button,
-  Typography
-} from '@material-ui/core';
-import {
-  Link
-} from 'react-router-dom';
+
+// Components
+import HomeNavBar from '../components/Home/HomeNavBar';
+
+// Utilities
+import imagePath from '../utilities/imagePath';
+
+const vigorLogoWhite = imagePath('Vigor-Logo-White.svg');
 
 const Home = () => {
   return (
     <>
-      <Typography
-        variant={'h1'}>
-        Home
-      </Typography>
-      <Button
-        component={Link}
-        to="/login">
-        Login
-      </Button>
+      <HomeNavBar
+        vigorLogoWhite={vigorLogoWhite} />
     </>
   );
 };
