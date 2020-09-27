a7942ef818000ce0c867eacd255322146165e94d
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-09-27 23:02:44 +0000

    Got setUser action in place

--- client/Index.jsx
+++ client/Index.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  Provider
+} from 'react-redux';
+import {
+  createStore
+} from 'redux';
+
+import rootReducer from './reducers/index';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
---
+++ client/reducers/index.js
@@ -0,0 +1,11 @@
+import {
+  combineReducers
+} from 'redux';
+
+import currentUser from './currentUser';
+
+const rootReducer = combineReducers({
+  currentUser
+});
+
+export default rootReducer;
